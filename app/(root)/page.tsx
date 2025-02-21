d1aa2ff01df127356ce92b7a4ee787366c3bf49f
MMH576 <monjorulmohammed@gmail.com>
2025-02-20 21:14:17 -0500

    completed integrating clerck for authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
   );
 }
