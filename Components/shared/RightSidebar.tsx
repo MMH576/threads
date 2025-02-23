d0eff47c8538d7745e44ce9a16d8cb30f3ff4021
MMH576 <monjorulmohammed@gmail.com>
2025-02-23 12:03:52 -0500

    completed right sidebar and edited a bit of topbar

--- Components/shared/RightSidebar.tsx
+++ Components/shared/RightSidebar.tsx
@@ -1,6 +1,17 @@
 const RightSidebar = () => {
     return (
-        <h1>RightSidebar</h1>
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-light-1 text-heading4-medium">
+                    Suggested Communities
+                </h3>
+            </div>
+            <div className="flex flex-1 flex-col justify-start">
+                <h3 className="text-light-1 text-heading4-medium">
+                    Suggested Users
+                </h3>
+            </div>
+        </section>
     )
 }
 
