1cdd809428ff7ac4468053ade55d385671c08661
MMH576 <monjorulmohammed@gmail.com>
2025-02-23 15:28:21 -0500

    fixed form color schemes

--- Components/ui/form.tsx
+++ Components/ui/form.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { Slot } from "@radix-ui/react-slot"
@@ -13,7 +11,7 @@ import {
 } from "react-hook-form"
 
 import { cn } from "@/lib/utils"
-import { Label } from "@/components/ui/label"
+import { Label } from "@/Components/ui/label"
 
 const Form = FormProvider
 
@@ -95,7 +93,7 @@ const FormLabel = React.forwardRef<
   return (
     <Label
       ref={ref}
-      className={cn(error && "text-destructive", className)}
+      className={cn(error && "text-red-500 dark:text-red-900", className)}
       htmlFor={formItemId}
       {...props}
     />
@@ -135,7 +133,7 @@ const FormDescription = React.forwardRef<
     <p
       ref={ref}
       id={formDescriptionId}
-      className={cn("text-sm text-muted-foreground", className)}
+      className={cn("text-sm text-slate-500 dark:text-slate-400", className)}
       {...props}
     />
   )
@@ -157,7 +155,7 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-sm font-medium text-destructive", className)}
+      className={cn("text-sm font-medium text-red-500 dark:text-red-900", className)}
       {...props}
     >
       {body}
--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -4,7 +4,7 @@ import { currentUser } from "@clerk/nextjs/server";
 async function Page() {
     const user = await currentUser();
 
-    const userInfo = {};
+    const userInfo = {};    
 
     const userData = {
         id: user?.id,
@@ -16,16 +16,16 @@ async function Page() {
     }
 
     return (
-        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20 bg-white">
-            <h1 className="head-text text-black">Onboarding</h1>
-            <p className="text-base-regular text-gray-600">Complete your profile now to use Threads</p>
-            <section className="mt-9 bg-gray-50 p-10">
-                <AccountProfile 
-                user={userData} 
-                btnTitle="Continue"
-                />
-            </section>
-        </main>
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+      <h1 className='head-text'>Onboarding</h1>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
+      </p>
+
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
+    </main>
     )
 }
 
