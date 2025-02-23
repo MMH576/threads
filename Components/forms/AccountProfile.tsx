29ce8273c2251ee8c1a3bd6f93f5574075fe3179
MMH576 <monjorulmohammed@gmail.com>
2025-02-23 15:53:05 -0500

    aded some files

--- Components/forms/AccountProfile.tsx
+++ Components/forms/AccountProfile.tsx
@@ -34,10 +34,10 @@ const accountProfile = ({user, btnTitle}: Props) => {
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
-            profile_photo: "",
-            name: "",
-            username: "",
-            bio: "",
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || "",
         },
     })
     const handleImage = (e: ChangeEvent, fieldChange: (value: string) => void) => {
